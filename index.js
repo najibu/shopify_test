ffa30ce82d3ed0cc8fbb22d0cbf61deb1743c7ad
najibu <ns.najibu@gmail.com>
2017-03-21 11:37:06 +0000

    Added products

--- index.js
+++ index.js
@@ -3,19 +3,24 @@
 */
 var shopifyAPI = require('shopify-node-api');
 
-// Set variables for our request
-var Shopify = new shopifyAPI({
+// ---- start Shopify
+var shopify = new shopifyAPI({
+  verbose: false,
   shop: 'najibu-shopify', // najibu-shopify.myshopify.com 
-  shopify_api_key: '800e670759d58706278c1e84c8a504cd', //  API key 
-  access_token: 'b6d7a1a1eeb740c0c4252ea3fd1d897b', // temporary token 
-  password: 'z8zgBtKG42Iw'
+  shopify_api_key: '8b92fad0f055130172cc145be1ab8cd8', //  API key 
+  access_token: 'c14e99a01e61a86241fc790b2d978c00', // API password
 });
+// ---- end Shopify
 
 var snapshot = {
-  lastSync: '2016-12-1 00:00:00'
+  lastSync: '2017-03-20 00:00:00'
 }
 
-Shopify.get('admin/products.json', function(err, data, headers){
+// Get all products
+shopify.get('/admin/products.json', function(err, data, headers){
+  if (err) {
+    return console.log(err);
+  }
   console.log(data); // Data contains product json information
   console.log(headers); // Headers returned from request
 });
