1af30e2253d01d55d9e1b01e9e620b2c8681ce1c
najibu <ns.najibu@gmail.com>
2017-03-22 11:01:38 +0000

    Hid keys

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 * Shopify node.js API
 */
 var shopifyAPI = require('shopify-node-api');
+var config = require('./config.json');
+
 
 // ---- start Shopify
 var shopify = new shopifyAPI({
   verbose: false,
-  shop: 'najibu-shopify', // najibu-shopify.myshopify.com 
-  shopify_api_key: '8b92fad0f055130172cc145be1ab8cd8', //  API key 
-  access_token: 'c14e99a01e61a86241fc790b2d978c00', // API password
+  shop: config.shop, // najibu-shopify.myshopify.com 
+  shopify_api_key: config.shopify_api_key, //  API key 
+  access_token: config.access_token, // API password
 });
 // ---- end Shopify
 
@@ -17,10 +19,15 @@ var snapshot = {
 }
 
 // Get all products
-shopify.get('/admin/products.json', function(err, data, headers){
+shopify.get('/admin/products.json', {limit: 250}, function(err, data, headers){
   if (err) {
     return console.log(err);
   }
   console.log(data); // Data contains product json information
   console.log(headers); // Headers returned from request
 });
+
+function callback(err, data, headers) {
+  var api_limit = headers['http_x_shopify_shop_api_call_limit'];
+  console.log( api_limit ); // "1/40"
+}
